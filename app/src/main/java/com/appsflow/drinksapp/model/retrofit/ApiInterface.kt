154a2325378802b8c6f7a4308b9c34bad731d0df
luxoff <dima4365@gmail.com>
2021-10-17 21:22:41 +0300

    Deleted RetrofitClient.kt, appended ApiInterface.kt with retrofit companion object; Added RecyclerView to fragment_ordinary_drink.xml, created DrinksListAdapter.kt and drink_list_item.xml

--- app/src/main/java/com/appsflow/drinksapp/model/retrofit/ApiInterface.kt
+++ app/src/main/java/com/appsflow/drinksapp/model/retrofit/ApiInterface.kt
@@ -2,15 +2,31 @@ package com.appsflow.drinksapp.model.retrofit
 
 import com.appsflow.drinksapp.model.Drink
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiInterface {
 
     @GET("api/json/v1/{API_KEY}/filter.php?c=Ordinary_Drink")
-    fun getOrdinaryDrinks(@Path("API_KEY") apiKey: String) : Call<List<Drink>>
+    suspend fun getOrdinaryDrinks(@Path("API_KEY") apiKey: String) : Call<List<Drink>>
 
     @GET("api/json/v1/{API_KEY}/filter.php?c=Cocktail")
-    fun getCocktails(@Path("API_KEY") apiKey: String) : Call<List<Drink>>
+    suspend fun getCocktails(@Path("API_KEY") apiKey: String) : Call<List<Drink>>
 
+    companion object {
+        private var retrofit: Retrofit? = null
+        private const val BASE_URL = "https://thecocktaildb.com/"
+
+        fun getClient(): Retrofit {
+            if (retrofit == null) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit!!
+        }
+    }
 }
---
+++ app/src/main/java/com/appsflow/drinksapp/view/adapter/DrinksListAdapter.kt
@@ -0,0 +1,44 @@
+package com.appsflow.drinksapp.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.appsflow.drinksapp.databinding.DrinkListItemBinding
+import com.appsflow.drinksapp.model.Drink
+import com.squareup.picasso.Picasso
+
+class DrinksListAdapter(dataSet: List<Drink>) :
+    RecyclerView.Adapter<DrinksListAdapter.ViewHolder>() {
+
+    private val drinks = dataSet
+
+    class ViewHolder(val binding: DrinkListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        val binding = DrinkListItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val drink = drinks[position]
+
+        holder.binding.tvTitle.text = drink.strDrink
+        Picasso.get()
+            .load(drink.strDrinkThumb)
+            .resize(86, 86)
+            .centerCrop()
+            .into(holder.binding.ivImage)
+    }
+
+    override fun getItemCount(): Int {
+        return drinks.size
+    }
+
+}
+
+
